2fda994877d16a5245e013edd75caee69ab5dd77
Jan Cepelak <cepelak.honza@gmail.com>
2018-02-17 00:32:16 +0100

    Vše do lekce 50

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
       {name: 'John', age: 20},
       {name: 'George', age: 21},
       {name: 'Tom', age: 19}
-    ]
+    ],
+    peopleVisible: false
   }
   switchNameHandler = (newName) => {
     //console.log('clicked');
@@ -28,6 +29,13 @@ class App extends Component {
     ]});
   }
 
+  togglePersonHandler = () => {
+    const isVisible = this.state.peopleVisible;
+    this.setState({
+      peopleVisible: !isVisible
+    });
+  }
+
   render() {
     const style = {
       backgroundColor: "#ff9cad",
@@ -36,21 +44,33 @@ class App extends Component {
       padding: "12px",
       cursor: "pointer"
     };
+    
+    // v Reactu není možné v render funkci používat blokové podmínky přímo v return metodě
+
+    // pokud se má používat normální if/else blok, je to možné udělat takto ↓
+    let persons = null;
+    if (this.state.peopleVisible) {
+      persons = (
+        <div>
+          <Person name={this.state.people[0].name} age={this.state.people[0].age} />
+          <Person 
+            name={this.state.people[1].name}
+            age={this.state.people[1].age} 
+            click={this.switchNameHandler.bind(this, "Chorhe")}
+            changeName={this.nameChangedHandler.bind(this)}
+            >
+            I like trains.
+          </Person>
+          <Person name={this.state.people[2].name} age={this.state.people[2].age} />
+        </div>
+      );
+    }
 
     return (
       <div className="App">
         <h1> Is it working ? </h1>
-        <button style={style} onClick={this.switchNameHandler.bind(this, "Johanness")}>Switch Names</button>
-        <Person name={this.state.people[0].name} age={this.state.people[0].age} />
-        <Person 
-          name={this.state.people[1].name}
-          age={this.state.people[1].age} 
-          click={this.switchNameHandler.bind(this, "Chorhe")}
-          changeName={this.nameChangedHandler.bind(this)}
-          >
-          I like trains.
-        </Person>
-        <Person name={this.state.people[2].name} age={this.state.people[2].age} />
+        <button style={style} onClick={this.togglePersonHandler}>Show/Hide People</button>
+        {persons}
       </div>
     );
   }
